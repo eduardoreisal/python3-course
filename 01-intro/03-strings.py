5a84a164e6d5b0a15d4f0bb9f081e98c5ac8f882
Edu <dureisal1994@gmail.com>
2022-11-21 22:40:11 -0300

    Add Strings

---
+++ 01-intro/03-strings.py
@@ -0,0 +1,7 @@
+# String is text inside '' or ""
+print('Simple String')
+print('Other String')
+
+# To use special characters inside strings you need to use \
+print("This is an example\" :) ")
+# If you don't use \, you will receive an error
