f851037b0e5203d1bfa3e0b9df9fbb04160746ab
Edu <dureisal1994@gmail.com>
2022-11-21 22:04:26 -0300

    Add 01-intro comments

---
+++ 01-intro/01-comments.py
@@ -0,0 +1,7 @@
+# This is a single line comment
+print('This will be printed')
+
+'''
+This is a docstring, a this is not a comment
+but you can use it to create notes about functions, calsses, etc.
+'''
