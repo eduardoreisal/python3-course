e187054f400284761e0939a43cbe704f4be549b6
Edu <dureisal1994@gmail.com>
2022-11-23 19:50:57 -0300

    add class 04 int and float

---
+++ 01-intro/04-int-and-float.py
@@ -0,0 +1,14 @@
+# Any num positive or negative without: ".", "," is an it
+print(11)
+print(-11)
+print(0)
+
+# Float any number with: ",", "."
+print(1.1)
+print(0.25)
+print(-1.35)
+
+# To discover if a num is int or float you can use type
+print(type(0.01))
+print(type('Eduardo'))
+print(type(0))
