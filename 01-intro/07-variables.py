8e71b2be9084f3414abf334ed63a432e9fd6f8cb
Edu <dureisal1994@gmail.com>
2022-11-23 20:32:17 -0300

    add class-07 varables

---
+++ 01-intro/07-variables.py
@@ -0,0 +1,10 @@
+# You can assign values to variables and call these variables later
+name = "Eduardo"
+print(name)
+
+sum = 10 + 10
+print(sum)
+
+idade = 28
+maior_de_idade = idade >= 18
+print(f'Maior de idade? {maior_de_idade}')
