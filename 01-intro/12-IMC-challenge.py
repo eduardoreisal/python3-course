0ba50e878791cdd85f3f1b1a3c2a469a88b845c7
Edu <dureisal1994@gmail.com>
2022-11-24 14:17:32 -0300

    add class 12 imc challenge

---
+++ 01-intro/12-IMC-challenge.py
@@ -0,0 +1,13 @@
+name = 'eduardo'
+height = 1.80
+weight = 100
+imc = weight / height ** 2
+
+
+print(imc)
+
+print(f"""
+{name} tem {height} de altura,
+pesa {weight} quilos e seum IMC e:
+{imc}
+""")
