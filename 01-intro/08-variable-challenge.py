ff0a661f2a7e22bc5c52ae772eed46605cc454be
Edu <dureisal1994@gmail.com>
2022-11-24 13:46:28 -0300

    add class 08 variable challenge

---
+++ 01-intro/08-variable-challenge.py
@@ -0,0 +1,13 @@
+nome = 'Eduardo'
+sobrenome = 'Reis'
+idade = 28
+ano_nascimento = 1994
+maior_de_idade = idade >= 18
+altura_em_metros = 1.80
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('Maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_em_metros)
