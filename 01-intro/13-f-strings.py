151441ff10a8578c52b6f1776796f7799a329457
Edu <dureisal1994@gmail.com>
2022-11-24 14:33:58 -0300

    add class 13 f strings

---
+++ 01-intro/13-f-strings.py
@@ -0,0 +1,9 @@
+var1 = 'you'
+var2 = 'can'
+var3 = 'format'
+
+print(f'{var1} {var2} use f strings to {var3} text easily')
+
+num = 1.800000
+# You can use f strings to format decimal numbers
+print(f'{num:.2f}')
